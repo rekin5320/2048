a4c458dc00caecc581dafe4615b3731c2937c9f5
rekin5320 <mmach331@gmail.com>
2022-01-17 08:33:25 +0100

    Colors

--- 2048.py
+++ 2048.py
@@ -19,13 +19,14 @@ def main_loop():
 
 
 def redraw():
-    window.fill((150, 150, 150))
+    window.fill((250, 248, 239))
 
     for r, row in enumerate(Grid):
         for c, value in enumerate(row):
-            pygame.draw.rect(window, (10 * value % 255, 15 * value % 255, 20 * value % 255), (100 * c, 100 * r, 100, 100))
-            text = font.render(str(value), True, (0,200,0))
-            window.blit(text, (100 * c, 100 * r))
+            pygame.draw.rect(window, cell_color[value], (100 * c, 100 * r, 100, 100))
+            if value:
+                text = font.render(str(value), True, font_color[value])
+                window.blit(text, (100 * c, 100 * r))
 
 
 
@@ -37,8 +38,29 @@ pygame.font.init()
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((800, 700))
 
-font = pygame.font.SysFont("Verdana", 14)
-Grid = [[2, 4, 8, 0], [0, 16, 64, 1], [1024, 32, 0, 4], [0, 16, 8, 1]]
+font = pygame.font.SysFont("Verdana", 35)
+Grid = [[2, 4, 8, 0], [0, 16, 64, 8], [128, 32, 0, 4], [0, 16, 8, 64]]
+
+cell_color = {
+    0: (205, 193, 180),
+    2: (238, 228, 218),
+    4: (237, 224, 200),
+    8: (242, 177, 121),
+    16: (245, 149, 99),
+    32: (246, 124, 95),
+    64: (246, 94, 59),
+    128: (237, 207, 114),
+}
+
+font_color = {
+    2: (119, 110, 98),
+    4: (119, 110, 98),
+    8: (249, 246, 242),
+    16: (249, 246, 242),
+    32: (249, 246, 242),
+    64: (249, 246, 242),
+    128: (249, 246, 242),
+}
 
 main_loop()
 
